4bf642275349dcb9ed02a1fc7520f8ca659c2d02
Evie-Mututa <evemututa29@gmail.com>
2023-10-11 21:25:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  const messages = []; // Initialize an empty array to store the messages
+
+  for (let i = 0; i < names.length; i++) {
+    // Create a message for each person
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message); // Add the message to the messages array
+  }
+
+  return messages; // Return the array of messages
+}
+function countDown(number) {
+    // Start from the provided number and count down to zero
+    for (let i = number; i >= 0; i--) {
+      console.log(i); // Log each number when counting down
+    }
+  }
